0730a3de96e4a6d9186eca6fbce83f87f1293af9
maodaishan <maodaishan@163.com>
2023-10-17 16:06:21 +0800

    fix compile error

--- op-service/txmgr/txmgr.go
+++ op-service/txmgr/txmgr.go
@@ -11,6 +11,7 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/txpool"
